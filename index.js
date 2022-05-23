54a6d5ecc4f03e8b97496d0b0312cbcda78071fc
longleh <33480010+longleh@users.noreply.github.com>
2022-05-23 14:23:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // @name         Pokeclicker Bot
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  simple bot injected on pokéclicker
+// @author       longleh
 // @match        https://www.pokeclicker.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pokeclicker.com
 // @grant        unsafeWindow
