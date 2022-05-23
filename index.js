771f28cfd079d8257f5feb7c893d550afcde3e81
longleh <33480010+longleh@users.noreply.github.com>
2022-05-23 20:28:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,86 +9,101 @@
 // @grant        unsafeWindow
 // ==/UserScript==
 
-
 const htmlArrayToArray = (htmlArray) => {
-    if (htmlArray.length === 0) return [];
-    return [...htmlArray];
+  if (htmlArray.length === 0) return [];
+  return [...htmlArray];
+};
+
+const logger = (message, on = true) => {
+  const color = on ? "green" : "red";
+  console.log(`%c ${message}`, `color: ${color}`);
+};
+
+(function () {
+  "use strict";
+  let autoClickInterval;
+  let autoBreedingInterval;
+  let numberOfBreed = 0;
+  const enemy = document.getElementsByClassName("enemy")?.[0];
+  const autoBreedingEventHandler = document.createElement("div");
+
+  const recursiveBreed = (pkmnIndex, pkmnToHatch, breedModal) => {
+    if (
+      pkmnIndex < numberOfBreed &&
+      breedModal.classList.contains("active") &&
+      pkmnIndex < pkmToHatch.length
+    ) {
+      pkmToHatch[pkmnIndex].children[4].click();
+      setTimeout(
+        () => recursiveBreed(pkmnIndex++, pkmnToHatch, breedModal),
+        1000
+      );
+    } else {
+      logger("BREEDING: END", false);
+      return "OK";
+    }
   };
-  
-  const logger = (message, on = true) => {
-    const color = on ? "green" : "red";
-    console.log(`%c ${message}`, `color: ${color}`);
+
+  const breed = () => {
+    const pkmToHatch = htmlArrayToArray(
+      document.querySelectorAll("li.eggSlot:not([style*=display])")
+    );
+    logger("BREEDING: START");
+    const breedModal = document.getElementById("breeding-pokemon");
+    recursiveBreed(0, pkmToHatch, breedModal);
+    numberOfBreed = 0;
   };
-  
-  
-  (function () {
-    "use strict";
-    let autoClickInterval;
-    let autoBreedingInterval;
-    const enemy = document.getElementsByClassName("enemy")?.[0];
-  
-    const autoclick = (time = 10) => {
-      logger("AUTOCLICK: ON");
-      autoClickInterval = setInterval(() => {
-        enemy.click();
-      }, time);
-      return autoClickInterval;
-    };
-  
-    const stop = () => {
-      clearInterval(autoClickInterval);
-      logger("AUTOCLICK: OFF", false);
-    };
-  
-    const autoBreedingFunc = () => {
-      logger("AUTOBREEDING: ON");
-      autoBreedingInterval = setInterval(() => {
-        const eggs = htmlArrayToArray(
-          document.getElementsByClassName("hatching")
-        );
-        let hatchedEggs = 0;
-        if (eggs.length > 0) {
-          eggs.forEach((egg) => egg.click());
-          hatchedEggs++;
-        }
-        if (hatchedEggs > 0) {
-          // open day care
-          document
-            .querySelector("[data-town='Pokemon Day Care']")
-            .dispatchEvent(new Event("click"));
-          setTimeout(() => {
-            const pkmToHatch = htmlArrayToArray(
-              document.querySelectorAll('li.eggSlot:not([style*=display])')
-            );
-              const breedModal = document.getElementById('breeding-pokemon');
-            for (
-              let pkmnIndex = 0;
-              document.querySelectorAll('.egg').length < 4 &&
-              breedModal.classList.contains('active') &&
-              pkmnIndex < pkmToHatch.length;
-              pkmnIndex++
-            ) {
-              // 4th children is the click event handler
-                pkmToHatch[pkmnIndex].children[4].click()
-            }
-          }, 3000);
-        }
-        hatchedEggs = 0;
-      }, 10000);
-    };
-  
-    const stopAutoBredding = () => {
-      clearInterval(autoBreedingInterval);
-      logger("AUTOBREEDING: OFF", false);
-    };
-  
-    unsafeWindow.autoclick = autoclick;
-    unsafeWindow.stop = stop;
-    unsafeWindow.autobreeding = autoBreedingFunc;
-    unsafeWindow.stopAutobreeding = stopAutoBredding;
-    unsafeWindow._a = autoclick;
-    unsafeWindow._s = stop;
-    unsafeWindow._ab = autoBreedingFunc;
-    unsafeWindow._sab = stopAutoBredding;
-  })();
-  
+
+  autoBreedingEventHandler.addEventListener("breed", breed);
+
+  const autoclick = (time = 10) => {
+    logger("AUTOCLICK: ON");
+    autoClickInterval = setInterval(() => {
+      enemy.click();
+    }, time);
+    return autoClickInterval;
+  };
+
+  const stop = () => {
+    clearInterval(autoClickInterval);
+    logger("AUTOCLICK: OFF", false);
+  };
+
+  const autoBreedingFunc = () => {
+    logger("AUTOBREEDING: ON");
+    autoBreedingInterval = setInterval(() => {
+      const eggs = htmlArrayToArray(
+        document.getElementsByClassName("hatching")
+      );
+      let hatchedEggs = 0;
+      if (eggs.length > 0) {
+        eggs.forEach((egg) => egg.click());
+        hatchedEggs++;
+      }
+      if (hatchedEggs > 0) {
+        // open day care
+        document
+          .querySelector("[data-town='Pokemon Day Care']")
+          .dispatchEvent(new Event("click"));
+        setTimeout(() => {
+          autoBreedingEventHandler.dispatchEvent(new Event("breed"));
+        }, 3000);
+      }
+      hatchedEggs = 0;
+    }, 10000);
+  };
+
+  const stopAutoBredding = () => {
+    clearInterval(autoBreedingInterval);
+    logger("AUTOBREEDING: OFF", false);
+  };
+
+  unsafeWindow.autoclick = autoclick;
+  unsafeWindow.stop = stop;
+  unsafeWindow.autobreeding = autoBreedingFunc;
+  unsafeWindow.stopAutobreeding = stopAutoBredding;
+  unsafeWindow._a = autoclick;
+  unsafeWindow._s = stop;
+  unsafeWindow._ab = autoBreedingFunc;
+  unsafeWindow._sab = stopAutoBredding;
+})();
