e9a7b7015ca4329d7d55b954bd6b65748a070e4d
longleh <33480010+longleh@users.noreply.github.com>
2022-05-24 00:43:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const logger = (message, on = true) => {
 
   autoBreedingEventHandler.addEventListener("breed", breed);
 
-  const autoclick = (time = 10) => {
+  const autoclick = (time = 60) => {
     logger("AUTOCLICK: ON");
     autoClickInterval = setInterval(() => {
       enemy.click();
@@ -100,6 +100,66 @@ const logger = (message, on = true) => {
     clearInterval(autoBreedingInterval);
     logger("AUTOBREEDING: OFF", false);
   };
+
+  const generateInput = (label, onListener, offListener) => {
+    const inputContainer = document.createElement("div");
+    inputContainer.style.display = 'flex'
+    inputContainer.style.margin = '8px'
+    inputContainer.style.justifyContent = 'space-evenly'
+    const span = document.createElement("span");
+    span.innerText = label;
+    const checkbox = document.createElement("input");
+    checkbox.type = 'checkbox'
+    checkbox.addEventListener("change", (event) => {
+      if (event.currentTarget.checked) {
+        onListener();
+      } else {
+        offListener();
+      }
+    });
+    inputContainer.appendChild(span);
+    inputContainer.appendChild(checkbox);
+    return inputContainer
+  };
+
+  const generateInterface = () => {
+    const container = document.createElement("div");
+    container.className =
+      "pokebot card sortable border-secondary mb-3 sortable-chosen";
+    container.style.zIndex = "999";
+    const cardHeader = document.createElement("div");
+    cardHeader.className = "card-header p-0";
+    const cardHeaderText = document.createElement("span");
+    cardHeaderText.innerText = "Pokeclicker Bot";
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body p-0 show";
+    const autoclickInput = generateInput('Autoclick', autoclick, stop)
+    const autoBreedingInput = generateInput('AutoBreeding', autoBreedingFunc, stopAutoBredding)
+    cardHeader.appendChild(cardHeaderText);
+    container.appendChild(cardHeader);
+    cardBody.appendChild(autoclickInput)
+    cardBody.appendChild(autoBreedingInput)
+    container.appendChild(cardBody);
+
+    document.getElementById("right-column").appendChild(container);
+  };
+
+  const waitForGame = () => {
+    const intervalGameId = setInterval(() => {
+      if (
+        unsafeWindow.getComputedStyle(document.getElementById("game"))
+          .display !== "none"
+      ) {
+        logger("GENERATING INTERFACE");
+        generateInterface();
+        clearInterval(intervalGameId);
+      } else {
+        logger("WAITING FOR THE GAME TO LAUNCH INTERFACE", false);
+      }
+    }, 5000);
+  };
+
+  waitForGame();
 
   unsafeWindow.autoclick = autoclick;
   unsafeWindow.stop = stop;
